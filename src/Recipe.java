58c87c547305281c8db0f27d315f930d9add9a64
yu1ro <tresener.yu1ro@gmail.com>
2015-06-06 11:31:58 +0900

    spec3:ファイルを読み込み

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,5 +1,13 @@
+import java.io.*;
+
 public class Recipe {
-    public static void main(String args[]) {
-        System.out.println("オムライス");
+    public static void main(String args[]) throws IOException {
+        //ファイル読み込み
+        File recipe_file = new File("data/recipe_data.txt");
+        FileReader recipe_reader = new FileReader(recipe_file);
+        BufferedReader recipe_buff = new BufferedReader(recipe_reader);
+
+        String str = recipe_buff.readLine();
+        System.out.println(str);
     }
 }
